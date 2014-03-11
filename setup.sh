0130e003e13e3a12c374fd6b7756adaed2ae9d32
Chris DiMartino <chris.dimartino@gmail.com>
2014-03-11 17:07:35 -0400

    Renaming script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 PACKAGES="python-devel gcc cloog-ppl cpp glibc-devel libgomp glibc-headers mpfr ppl kernel-headers python-pip"
 yum -y install $PACKAGES
 yum install python-pip
-pip install bakthat 
+pip install bakthat argparse importlib
 yum -y erase $PACKAGES
 
 useradd dbbackup
