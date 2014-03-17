72614168be2f96c7cf0d4b62cd6f44a3b974a3ec
Chris DiMartino <chris.dimartino@gmail.com>
2014-03-17 15:26:43 -0400

    Attempt to monkey patch backend

--- mp_s3_backend.py
+++ mp_s3_backend.py
@@ -14,6 +14,7 @@ import boto
 from multiprocessing.pool import IMapIterator
 import rfc822
 
+log = logging.getLogger(__name__)
 
 def map_wrap(f):
   @functools.wraps(f)
@@ -112,4 +113,4 @@ class MP_S3Backend(S3Backend):
 class S3Swapper(Plugin):
   def activate(self):
     bakthat.STORAGE_BACKEND = dict(s3=MP_S3Backend)
-    self.log.info('Replaced S3Backend with MP_S3Backend')
+    log.info('Replaced S3Backend with MP_S3Backend')
