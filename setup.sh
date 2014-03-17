098a89eca7c5cb98d8b06db4102d1880f6cbc535
Chris DiMartino <chris.dimartino@gmail.com>
2014-03-17 09:46:42 -0400

    Adding multiprocessing backend to bakthat

--- setup.sh
+++ setup.sh
@@ -13,11 +13,19 @@ if [ $? -ne 0 ]
   getent passwd dbbackup >/dev/null || useradd dbbackup
 fi
 
-if [ ! -f ~/.bakthat.yml ]
+if [ ! -f $HOME/.bakthat.yml ]
   then
   sudo -u dbbackup bakthat configure
   sudo -u dbbackup bakthat configure_backups_rotation
+
+  echo <<EOL >> $HOME/.bakthat.yml
+  plugins_dir: $HOME/.bakthat_plugins/
+  plugins: [mp_s3_backend.S3Swapper]
+EOL
 fi
 
 cp -a dbbackup.sh /usr/local/bin/
 cp -a dbrestore.sh /usr/local/bin/
+
+mkdir -f $HOME/.bakthat_plugins
+cp -a mp_s3_backend.py $HOME/.bakthat_plugins/
