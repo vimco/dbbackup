bb176d06a48b7b8c6ac07b8ab8715bf36c203bd0
Chris DiMartino <chris.dimartino@gmail.com>
2014-04-03 12:25:53 -0400

    Make setup work for debian as well as RH

--- setup.sh
+++ setup.sh
@@ -2,28 +2,62 @@
 
 USER_HOME=/home/dbbackup
 
-which bakthat > /dev/null 2>&1
-if [ $? -ne 0 ]
-  then
-  PACKAGES="python-devel gcc cloog-ppl cpp glibc-devel libgomp glibc-headers mpfr ppl kernel-headers python-pip"
-  yum -y install $PACKAGES
-  # download and install xtradbbackup
-  yum -y install python-pip pigz
-  pip install bakthat argparse importlib
-  yum -y erase $PACKAGES
-
-  getent passwd dbbackup >/dev/null || useradd dbbackup
+/bin/ls /etc/debian_version >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  OS='redhat'
+else
+  OS='debian'
 fi
 
-which innobackupex > /dev/null 2>&1
-if [ $? -ne 0 ]
+install_bakthat() {
+  if [ "$OS" == 'debian' ];
   then
+    PACKAGES="build-essential python-pip"
+    apt-get -y install $PACKAGES pigz
+    # download and install xtradbbackup
+    pip install bakthat argparse importlib
+    apt-get -y remove $PACKAGES
+  else
+    PACKAGES="python-devel gcc cloog-ppl cpp glibc-devel libgomp glibc-headers mpfr ppl kernel-headers python-pip"
+    yum -y install $PACKAGES pigz
+    # download and install xtradbbackup
+    pip install bakthat argparse importlib
+    yum -y erase $PACKAGES
+  fi
+
+  getent passwd dbbackup >/dev/null || useradd dbbackup
+}
+
+install_xtrabackup() {
   pushd /tmp
-  wget http://mirror-fpt-telecom.fpt.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
-  yum install -y epel-release-6-8.noarch.rpm
-  wget http://www.percona.com/redir/downloads/XtraBackup/LATEST/RPM/rhel6/x86_64/percona-xtrabackup-2.1.8-733.rhel6.x86_64.rpm
-  yum install -y percona-xtrabackup-2.1.8-733.rhel6.x86_64.rpm
+  if [ "$OS" == "debian" ]
+  then
+    wget http://www.percona.com/redir/downloads/XtraBackup/LATEST/deb/wheezy/x86_64/percona-xtrabackup_2.1.8-733-1.wheezy_amd64.deb
+    dpkg -i percona-xtrabackup_2.1.8-733-1.wheezy_amd64.deb
+    if [ $? -ne 0 ]
+    then
+      apt-get -fy install
+      dpkg -i percona-xtrabackup_2.1.8-733-1.wheezy_amd64.deb
+    fi
+    rm -f percona-xtrabackup_2.1.8-733-1.wheezy_amd64.deb
+  else
+    wget http://mirror-fpt-telecom.fpt.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+    yum install -y epel-release-6-8.noarch.rpm
+    wget http://www.percona.com/redir/downloads/XtraBackup/LATEST/RPM/rhel6/x86_64/percona-xtrabackup-2.1.8-733.rhel6.x86_64.rpm
+    yum install -y percona-xtrabackup-2.1.8-733.rhel6.x86_64.rpm
+    rm -f percona-xtrabackup-2.1.8-733.rhel6.x86_64.rpm
+  fi
   popd
+}
+
+which bakthat > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  install_bakthat
+fi;
+
+which innobackupex > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  install_xtrabackup
 fi
 
 if [ ! -f $USER_HOME/.bakthat.yml ]
